8d259140c6f539a609aa34c73dbb171015e5547c
yfurukawa <yo4.furukawa+github@gmail.com>
2015-01-08 23:48:14 +0900

    Add main

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * make.cpp
+ *
+ *  Created on: 2015/01/08
+ *      Author: yoshi
+ */
+#include "Conductor.h"
+
+int main(int argc, char* argv[]) {
+    Conductor conductor_;
+    conductor_.execute();
+
+    return 0;
+}
+
