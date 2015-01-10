a511ad78e8ddbea6a06bf9a28e5f772679ecf617
yfurukawa <yo4.furukawa+github@gmail.com>
2015-01-10 18:58:36 +0900

    ネットワーク用の例外クラスの追加

---
+++ src/NetworkException.cpp
@@ -0,0 +1,15 @@
+/*
+ * NetworkException.cpp
+ *
+ *  Created on: 2015/01/10
+ *      Author: yoshi
+ */
+
+#include "NetworkException.h"
+
+NetworkException::NetworkException() {
+}
+
+NetworkException::~NetworkException(){
+}
+
---
+++ src/NetworkException.h
@@ -0,0 +1,20 @@
+/*
+ * NetworkException.h
+ *
+ *  Created on: 2015/01/10
+ *      Author: yoshi
+ */
+
+#ifndef NETWORKEXCEPTION_H_
+#define NETWORKEXCEPTION_H_
+
+#include <bits/ios_base.h>
+#include <string>
+
+class NetworkException: public std::ios_base::failure {
+    public:
+        NetworkException(const std::string& cause)
+            : std::ios_base::failure("cause : " + cause) {}
+};
+
+#endif /* NETWORKEXCEPTION_H_ */
