570e89a693d03ca0b64ea844d3f9645449fb3038
yfurukawa <yo4.furukawa+github@gmail.com>
2015-01-18 17:39:45 +0900

    共通定義を別出しにした

---
+++ src/definition.h
@@ -0,0 +1,17 @@
+/*
+ * definition.h
+ *
+ *  Created on: 2015/01/18
+ *      Author: yoshi
+ */
+
+#ifndef DEFINITION_H_
+#define DEFINITION_H_
+
+enum status {SUCCESS, FAIL};
+
+#define ON 1
+#define OFF 0
+
+
+#endif /* DEFINITION_H_ */
